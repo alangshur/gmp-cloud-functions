7dc8481bef19820144ba88c61fa4e4a8bcae98ac
alangshur <adlangshur@gmail.com>
2020-03-21 13:14:04 -0500

    Setup initial user functions

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+var cors = require('cors')({ origin: true });
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+
+
+/*** USER FUNCTIONS ***/
+
+// (AUTH) add user to firestore upon creation
+exports.addUser = functions.auth.user().onCreate(user => {
+    return db.collection('users').doc(user.uid).set({
+        profile: {
+            username: '',
+            photoURL: '',
+            location: ''
+        }
+    });
+});
