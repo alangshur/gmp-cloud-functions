956e865415926c8dc960b789ff29a894cb9333d9
alangshur <adlangshur@gmail.com>
2020-03-23 14:58:09 -0500

    Added survey udpate function

--- functions/index.js
+++ functions/index.js
@@ -15,5 +15,61 @@ exports.addUser = functions.auth.user().onCreate(user => {
         id: user.uid,
         name: user.displayName,
         email: user.email,
+
+        surveyAnswers: null,
+        lastSignup: null,
+        currentMatch: null
     });
+});
+
+
+
+/*** SURVEY FUNCTIONS ***/
+
+// (HTTP) update user's survey answers
+exports.updateSurvey = functions.https.onRequest((request, response) => {
+    try {
+
+        // handle preflight
+        if (request.method == 'OPTIONS') {
+            return cors(request, response, () => {
+                response.send({ success: true });
+            });
+        }
+
+        // enforce authentication
+        var userId = undefined;
+        const token = request.get('Authorization');
+        if (!token) throw new Error('auth-r');
+        await admin.auth().verifyIdToken(token).then(
+            decoded => { userId = decoded.user_id; },
+            err => { throw new Error('auth-f'); }
+        );
+
+        // fetch survey data
+        const surveyAnswers = request.get('Survey');
+        console.log(surveyAnswers);
+
+        // return https response
+        cors(request, response, () => {
+            response.send({ success: true });
+        });
+    }
+    catch (err) {
+        console.log('updateSurvey: ' + err);
+        var message = '';
+
+        // select error message
+        switch (err.message) {
+            case 'auth-r': message = 'Error authenticating user. Please refresh page.'; break;
+            case 'auth-f': message = 'Error authenticating user. Please refresh page.'; break;
+        }
+
+        cors(request, response, () => {
+            response.send({
+                success: false,
+                message: message
+            });
+        });
+    }
 });
